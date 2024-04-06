e67ea8e6cca07cf79eb61e62c1c51e2e0f03cccf
Sviatoslav Pohrebnyi <svyatoslav021200@gmail.com>
2024-04-06 23:17:47 +0300

    Initial commit

---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public int Health
+    {
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _health = value;
+            if (value <= 0)
+            {
+                _health = 0;
+                Debug.Log(message: "Health 0");
+            }
+            else if (value >= 100)
+            {
+                _health = 100;
+                Debug.Log(message: "Health 100");
+            }
+          
+        }
+    }
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    private string _name = "Person";
+    private int _health;
+   
+    public void Start()
+    {
+        Health = -110;
+        Debug.Log(message: $"Health: {Health}");
+        Debug.Log(message: $"Name: {Name}");
+    }
+    
+}
